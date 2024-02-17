1976891d733b7c322a35f7a4a2568395eb338a8c
surelyli <shirley.li@sjsu.edu>
2024-02-17 13:19:34 -0800

    tamagui

--- my-app/screens/signup.screen.jsx
+++ my-app/screens/signup.screen.jsx
@@ -1,19 +1,72 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { Button } from 'tamagui';
+import { View, StyleSheet, FlatList } from "react-native";
+import { Button, Text, Input } from "tamagui";
 
 const Signup = ({ navigation }) => {
-  const [posts, setPosts] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, showErrorMessage] = useState(false);
+
+  const handleSignup = () => {
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword
+    };
+    if (userData.password === userData.confirmPassword) {
+        // proceed with making new user
+    } else {
+
+    }
+    console.log(userData);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.labelContainer}>
         <Text style={styles.label}>full name</Text>
+        <Input
+          size="$3"
+          style={styles.inputBox}
+          borderWidth={2}
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
         <Text style={styles.label}>email</Text>
+        <Input
+          size="$3"
+          style={styles.inputBox}
+          borderWidth={2}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
         <Text style={styles.label}>password</Text>
+        <Input
+          size="$3"
+          style={styles.inputBox}
+          borderWidth={2}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
         <Text style={styles.label}>confirm password</Text>
+        <Input
+          size="$3"
+          style={styles.inputBox}
+          borderWidth={2}
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
       </View>
-      <Button>next</Button>
+      <Button
+        style={styles.button}
+        onClick={handleSignup}
+      >
+        <Text style={styles.buttonText}>next</Text>
+      </Button>
+      
     </View>
   );
 };
@@ -21,38 +74,47 @@ const Signup = ({ navigation }) => {
 export default Signup;
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#B0C38F',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-    },
-    labelContainer: {
-      alignItems: 'flex-start',
-    },
-    label: {
-      textAlign: 'left',
-      marginBottom: 5,
-        fontFamily: 'Sora',
-        fontWeight: 500,
-        fontSize: "20px",
-        color: "#FBF6F1"
-    },
-    button: {
-      backgroundColor: '#FBF6F1',
-      padding: 10,
-      borderRadius: 7,
-      margin: 10,
-      width: 108,
-      height: 41,
-      textAlign: "center",
-      fontFamily: "Inter"
-    },
-    inputBox: {
-        width: "272px",
-        height: "29px",
-        background: "#FBF6F1",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: "7px"
-    }
+  container: {
+    backgroundColor: "#B0C38F",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+  labelContainer: {
+    alignItems: "flex-start",
+  },
+  label: {
+    textAlign: "left",
+    marginBottom: 5,
+    marginTop: 5,
+    fontFamily: "Sora",
+    fontWeight: 500,
+    fontSize: "20px",
+    color: "#FBF6F1",
+  },
+  buttonText: {
+    fontFamily: "Sora",
+    fontWeight: 500,
+    fontSize: "20px",
+    color: "#556B2F",
+  },
+  button: {
+    backgroundColor: "#FBF6F1",
+    padding: 10,
+    borderRadius: 7,
+    margin: 10,
+    width: 108,
+    height: 41,
+    textAlign: "center",
+    fontFamily: "Inter",
+    marginTop: 20,
+  },
+  inputBox: {
+    width: "272px",
+    height: "29px",
+    background: "#FBF6F1",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    borderRadius: "7px",
+    marginBottom: 20,
+  },
 });
